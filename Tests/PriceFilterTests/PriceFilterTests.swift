dbc8a888779e8c6271ff067f99b88bb1cfd62e22
Valerio <valdal14@gmail.com>
2024-03-07 15:22:51 +0100

    restore tooling to version 5.9

--- Tests/PriceFilterTests/PriceFilterTests.swift
+++ Tests/PriceFilterTests/PriceFilterTests.swift
@@ -120,7 +120,7 @@ final class PriceFilterTests: XCTestCase {
 	
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
 		let table = "Localizable"
-		let bundle = Bundle.main
+		let bundle = Bundle.module
 		let value = bundle.localizedString(forKey: key, value: nil, table: table)
 		if value == key {
 			XCTFail("Missing localised string for key: \(key) in table: \(table)", file: file, line: line)
